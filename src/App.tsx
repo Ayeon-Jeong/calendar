ae654479ae2e5ea6f007dfb825d71adb3b05cf97
jdh96 <jdh961224@gmail.com>
2023-04-09 22:24:58 +0900

    com

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Calendar from "react-calendar";
+import './components/calendar.css';
+import moment from "moment";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, onChange] = useState<Date>(new Date());
+
+    return (
+        <div>
+            <Calendar value={value} onChange={onChange}/>
+        </div>
+    );
 }
 
 export default App;
